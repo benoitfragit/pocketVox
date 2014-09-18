b16624131946a1e901ffbf1799b31b3468c798ae
benoitfragit <benoitfranquet@gmail.com>
2014-09-18 17:34:52 +0200

    * NEW [POCKETVOX] Beginning of the python wrapper
      - Adding the wrapper
      - Changing configure.ac and Makefile.am

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,3 @@
  #!/bin/sh
      autoreconf --force --install -I m4
+     ./configure --enable-introspection=yes
--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include "pocketvox_recognizer.h"
-#include "pocketvox_notifier.h"
-#include "pocketvox_dictionnary.h"
-#include "pocketvox_indicator.h"
-#include "pocketvox_controller.h"
+#include "pocketvox-recognizer.h"
+#include "pocketvox-notifier.h"
+#include "pocketvox-indicator.h"
+#include "pocketvox-controller.h"
 
 int main(int argc, char **argv)
 {
@@ -11,13 +10,12 @@ int main(int argc, char **argv)
 	gst_init(&argc, &argv);
 	
 	PocketvoxIndicator *indicator = pocketvox_indicator_new();
-	PocketvoxDictionnary* dict = pocketvox_dictionnary_new("/home/benoit/Bureau/dico.ini", TRUE);
 	PocketvoxNotifier *pn = pocketvox_notifier_new();
 	PocketvoxRecognizer *pr = pocketvox_recognizer_new("/home/benoit/Projet/voicerecognition/lium_french_f0",
 													   "/home/benoit/Projet/voicerecognition/lm/dictionnary.lm.dmp",
 													   "/home/benoit/Projet/voicerecognition/frenchWords62K.dic");	
 
-	PocketvoxController *ctr = pocketvox_controller_new(pr, pn, dict, indicator);
+	PocketvoxController *ctr = pocketvox_controller_new(pr, pn, indicator);
 	
 	pocketvox_controller_start(ctr);
 	
