33b6ce6d8ac9b46c38dea102de438399996f6419
benoitfragit <benoitfranquet@gmail.com>
2014-10-10 12:18:04 +0200

    * UPDATE [POCKETVOX] moving setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export ESPEAK_VOICES_PATH=/usr/lib/i386-linux-gnu/espeak-data/voices
-export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-0.10
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/include/espeak
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig
-export GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0:$GI_TYPELIB_PATH
